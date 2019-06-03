73024fa4f5d9fa2522319b744a6d9a9d7b742047
akbarkusuma32 <akbar.kusuma3212@users.noreply.github.com>
2019-06-03 20:53:56 +0700

    login pelanggan

---
+++ application/views/pelanggan/login.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="zxx">
+<title>Trajek Line | Login</title>
+<?php include('partials/title.php') ?>
+<body>
+	<!-- Page Preloder -->
+	<?php include('partials/preloader.php') ?>
+
+	<!-- Header section -->
+	
+	<!-- Header section end -->
+	<?php include('partials/menubar.php') ?>
+
+	<!-- Contact section end -->
+	<section class="cart-section-spad" style=" margin: 50px 100px; padding: 0px 400px" align="center">
+		
+			
+			
+			
+			
+                    <!-- Column -->
+                 
+                    <div style="margin-bottom: : 50px;">
+                        <div>
+                            <div class="card-body">
+                                <div class="contact-form" align="center">
+						<h3 style="margin-bottom: 20px;">Masuk</h3>
+						<input type="email" placeholder="E-mail">
+						<input type="password" placeholder="****">
+						<button class="site-btn">Login</button>
+						</ul>
+					</div>
+                            </div>
+                        </div>
+                    </div>
+	
+	</section>
+
+
+	
+
+
+	<!-- Footer section -->
+	
+	<!-- Footer section end -->
+	 <?php include('partials/modal.php') ?>
+
+
+
+	<!--====== Javascripts & Jquery ======-->
+	<?php include('partials/js.php') ?>
+
+	</body>
+</html>
